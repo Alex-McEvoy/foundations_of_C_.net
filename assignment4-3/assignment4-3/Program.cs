d0b4ae4201d6aa6faf4e87322aef687587790fb2
Tangled-tessellations <Tangled-tessellations@users.noreply.github.com>
2016-11-23 10:29:49 -0800

    Add files via upload

---
+++ assignment4-3/assignment4-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace assignment4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number of factorials to see them displayed below!");
+            int num = int.Parse(Console.ReadLine());
+            int result = 1;
+
+            for(int i = 1; i <= num; i++)
+            {
+                result *= i;
+
+                Console.WriteLine("{0}! = {1}", i, result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
